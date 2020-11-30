14b8dacb0734b1c12818506f39f9649d71aa3ad1
tenman <a.tenman@gmail.com>
2020-11-30 18:26:13 +0900

    new file:   js/block.js

---
+++ js/block.js
@@ -0,0 +1,148 @@
+wp.blocks.registerBlockVariation(
+        'core/columns', {
+            name: 'cta-block',
+            title: wp.i18n.__( 'CTA Block', 'emulsion' ),
+            icon: 'admin-appearance',
+            scope: ['inserter'],
+            innerBlocks: [
+                ['core/column', {}, [
+                        ['core/image'],
+                        ['core/heading', {level: 3, placeholder: 'Title', align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description'}],
+                        ['core/buttons']
+                    ]],
+                ['core/column', {}, [
+                        ['core/image'],
+                        ['core/heading', {level: 3, placeholder: 'Title'}],
+                        ['core/paragraph', {placeholder: 'description'}],
+                        ['core/buttons']
+                    ]],
+                ['core/column', {}, [
+                        ['core/image'],
+                        ['core/heading', {level: 3, placeholder: 'Title'}],
+                        ['core/paragraph', {placeholder: 'description'}],
+                        ['core/buttons']
+                    ]]
+            ],
+            attributes: {
+                className: 'emulsion-cta-block'
+            }
+        }
+);
+
+wp.blocks.registerBlockVariation(
+        'core/group', {
+            name: 'grid-group-2col',
+            title: wp.i18n.__( 'Group Grid 2', 'emulsion' ),
+            icon: 'admin-appearance',
+            scope: ['inserter'],
+            innerBlocks: [
+                ['core/group', {className:'size1of2'}, [
+                        ['core/image']
+                    ]],
+                ['core/group', {className: 'size1of2 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]]
+
+            ],
+            attributes: {
+                align: 'full',
+                className: 'grid'
+            }
+        }
+);
+
+wp.blocks.registerBlockVariation(
+        'core/group', {
+            name: 'grid-group-3col',
+            title: wp.i18n.__( 'Group Grid 3', 'emulsion' ),
+            icon: 'admin-appearance',
+            scope: ['inserter'],
+            innerBlocks: [
+                ['core/group', {className: 'size1of3 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of3 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of3 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]]
+
+            ],
+            attributes: {
+                align: 'full',
+                className: 'grid'
+            }
+        }
+);
+wp.blocks.registerBlockVariation(
+        'core/group', {
+            name: 'grid-group-4col',
+            title: wp.i18n.__( 'Group Grid 4', 'emulsion' ),
+            icon: 'admin-appearance',
+            scope: ['inserter'],
+            innerBlocks: [
+                ['core/group', {className: 'size1of4 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of4 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of4 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of4 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]]
+
+            ],
+            attributes: {
+                align: 'full',
+                className: 'grid'
+            }
+        }
+);
+wp.blocks.registerBlockVariation(
+        'core/group', {
+            name: 'grid-group-5col',
+            title: wp.i18n.__( 'Group Grid 5', 'emulsion' ),
+            icon: 'admin-appearance',
+            scope: ['inserter'],
+            innerBlocks: [
+                ['core/group', {className: 'size1of5 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of5 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of5 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                ['core/group', {className: 'size1of5 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]],
+                                ['core/group', {className: 'size1of5 centered'}, [
+                        ['core/heading', {level: 2, placeholder: 'Title',align: 'center'}],
+                        ['core/paragraph', {placeholder: 'description', align: 'center'}]
+                    ]]
+
+            ],
+            attributes: {
+                align: 'full',
+                className: 'grid'
+            }
+        }
+);
