f9ea1e4e7582146d2ae485dbbc85b652662f282f
tenman <a.tenman@gmail.com>
2021-03-24 11:03:21 +0900

    new file:   block-patterns/block-pattern-list-tab.php 	new file:   block-patterns/block-pattern-modal.php

---
+++ block-patterns/block-pattern-modal.php
@@ -0,0 +1,43 @@
+<?php
+
+$html = <<<HTML
+<!-- wp:buttons -->
+<div class="wp-block-buttons modal-open-link">
+	<!-- wp:button -->
+	<div class="wp-block-button">
+		<a class="wp-block-button__link" href="./#modal-group-1">modal link text</a>
+	</div>
+	<!-- /wp:button -->
+</div>
+<!-- /wp:buttons -->
+
+<!-- wp:group {"className":"emulsion-modal solid-border modal"} -->
+<div id="modal-group-1" class="wp-block-group emulsion-modal solid-border modal">
+	<div class="wp-block-group__inner-container">
+
+		<!-- wp:paragraph {"textAlign":"right","placeholder":"Panel Title","className":"emulsion-modal-title alignfull"} -->
+		<p class="has-text-align-right emulsion-modal-title">
+			<a href="./" class="modal-close-link">X</a>
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:group {"className":"emulsion-modal-content"} -->
+		<div class="wp-block-group emulsion-modal-content">
+			<div class="wp-block-group__inner-container">
+
+					<!-- wp:paragraph {"placeholder":"content"} -->
+					<p>content</p>
+					<!-- /wp:paragraph -->
+
+			</div>
+		</div>
+		<!-- /wp:group -->
+
+	</div>
+</div>
+<!-- /wp:group -->
+HTML;
+
+$html = str_replace( array(PHP_EOL,"\t"), array('',''), $html );
+
+return $html;
