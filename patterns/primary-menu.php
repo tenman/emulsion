323b975667423445a908ab03e93533ca4f34c70e
tenman <a.tenman@gmail.com>
2022-10-27 18:25:56 +0900

    new file:   block-patterns/block-template-default.php 	new file:   block-template-parts/comments.html 	new file:   patterns/primary-menu.php 	new file:   source/scss/fse-layout-on.scss 	new file:   styles/daybreak.json

---
+++ patterns/primary-menu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Primary Menu
+ * Slug: emulsion/primary-menu
+ * Categories: navigation, emulsion, recently-added
+ * Viewport Width: 1280
+ * Inserter: yes
+ * Keywords: shape
+ * Description: Primary Menu
+ */
+?>
+<!-- wp:navigation {"__unstableLocation":"primary","align":"full","className":"fse-primary sticky","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"nowrap"}} -->
+<!-- wp:navigation-link {"label":"Home","url":"#","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"Blog","url":"#","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"Contact","url":"#","kind":"custom","isTopLevelLink":true} /-->
+<!-- /wp:navigation -->
