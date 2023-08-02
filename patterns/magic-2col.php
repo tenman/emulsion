15dcc38b8b3685652cdc5b431612e79345c23d1a
tenman <a.tenman@gmail.com>
2023-08-02 10:39:32 +0900

    new file:   patterns/magic-2col.php 	new file:   patterns/magic-layout.php

---
+++ patterns/magic-2col.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Magic 2col
+ * Block Types: core/post-content
+ * Slug: emulsion/magic-2col
+ * Categories: recently-added, layout, emulsion
+ * Inserter: yes
+ * Keywords: page
+ * Description: 2column content pattern
+ */
+?>
+<!-- wp:group {"align":"full","className":"emulsion-pattern-magic-2col emulsion-base-layout-apply-globaly","layout":{"type":"constrained","contentSize":"1280px","wideSize":"1280px"}} -->
+<div class="wp-block-group alignfull emulsion-pattern-custom-layout emulsion-base-layout-apply-globaly"><!-- wp:columns {"align":""} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"70%","className":"is-style-main"} -->
+<div class="wp-block-column is-style-main" style="flex-basis:70%"><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"29.99%"} -->
+<div class="wp-block-column" style="flex-basis:29.99%"><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
---
+++ patterns/magic-layout.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Magic Layout
+ * Block Types: core/post-content
+ * Slug: emulsion/magic-layout
+ * Categories: recently-added, layout, emulsion
+ * Inserter: yes
+ * Keywords: page
+ * Description: Custom Layout Pattern
+ */
+?>
+<!-- wp:group {"align":"full","className":"emulsion-pattern-magic-layout emulsion-base-layout-apply-globaly","layout":{"type":"constrained","contentSize":"960px","wideSize":"1280px"}} -->
+<div class="wp-block-group alignfull emulsion-pattern-magic-layout emulsion-base-layout-apply-globaly"><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
